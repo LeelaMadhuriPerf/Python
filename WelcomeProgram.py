a83e137b3856994c3d6b9bfbc09829f7c4fd5a58
LeelaMadhuriPerf <50782562+LeelaMadhuriPerf@users.noreply.github.com>
2023-05-02 20:52:21 +0530

    add welcomeprogram

---
+++ WelcomeProgram.py
@@ -0,0 +1,27 @@
+2+3
+9-8
+4*5
+#write the code in python for  multiplication table
+8/4  #floating point representation and floating division
+5/2
+5//2 # // represents for integer division
+8+9-7 #combination of calculations
+8+9-
+8+2*3
+(8+2)*3
+2*2*2   
+2**3    #** is power
+10/2    # Quotient in floating point
+10//3   #Quotient in integer value
+10%3    #Reminder modulo opertor is for printing reminder
+'siddu' #prints '' as well
+print('siddu')  #printing the name only
+print('leela's mac')  #prints error as single quote is in between
+print("leela's mac ")   #solution to print the string
+print('leela "macbookpro"')  #to print the string in double quotes
+print('leela\'s "macbookpro"')  #\ is to ignore the special meaning of the single quotes
+'leela'+'leela'+'leela '
+10*'leela'
+print('c:\docs\niche')  #/n represents new line,to avoid that we use Raw String
+print(r'c:\docs\niche')
+
