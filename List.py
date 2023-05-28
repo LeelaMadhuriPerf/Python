6874aed06d4237ac1eb67c1e0a59a07ae8e57171
LeelaMadhuriPerf <50782562+LeelaMadhuriPerf@users.noreply.github.com>
2023-05-28 16:00:10 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,24 @@
+nums=[12,25,36,95,14]
+nums[0]
+nums[54\]
+nums[54]
+nums[2:]
+nums[-1]
+nums[-5]
+names=['lilly','madhu','lalee']
+names
+values=[9.5,'lalee',9]
+misc=[nums,names]
+misc
+nums.append(45)
+nums.insert(2, 77);
+nums
+nums.remove(14)
+nums.pop(1) #remove the value based on index value passed
+nums.pop()  #follows the concept of data structures,stack---->push and pop--Push means pushing the element into the stack,popping means removing the element from the stack
+nums
+del nums[2:]
+nums
+nums.extend([29,12,14,36])
+nums
+
