f305e4b2c7a7597d5b574c8247a80dae53855efe
LeelaMadhuriPerf <50782562+LeelaMadhuriPerf@users.noreply.github.com>
2023-05-30 22:17:04 +0530

    Tuple & Set Concepts in Python

---
+++ Tuples&set.py
@@ -0,0 +1,18 @@
+#List has different type of values like Integer,Float,String,Tuple is alomst similar to List,we have collection of values there,the difference is in list we can change value
+#because list is mutable,and tuple is immutable that means you cannot change the value,but where it can be used,let's try it out
+tup=(21,36,14,25);
+tup
+tup[1]
+tup[1]=33;
+tup.count#count the number of occurances of a number,in a tuple we have 10 values,5 is repeated 2 times,
+#()is for tuple,when u have list,we dont have to change the values,since we dont change,iteration is faster than list
+#set-set is a collection of unique elements,{} is for set
+# mathematical definition of set is it is a collection of unique elements
+#we are not sure about the sorting of set because set uses the concept of hash and using hash we use the improve the performance,we want to fetch the element as soon as possible
+#set does not allows duplicate values and doesnot follow sequence
+s={22,25,14,21,5}
+s
+s={25,14,98,63,75,98}
+
+
+
