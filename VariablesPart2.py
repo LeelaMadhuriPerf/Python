cbe1cb8ad63da8c80fcd3816f1e46a16d06dbcc2
LeelaMadhuriPerf <50782562+LeelaMadhuriPerf@users.noreply.github.com>
2023-06-02 00:54:44 +0530

    More on variables
    address,type of variables defined,constants

---
+++ VariablesPart2.py
@@ -0,0 +1,28 @@
+num=5
+id(num)
+name='shisha'
+id(name)
+a=10
+b=a
+a
+b
+id(a)
+id(b)
+#In python if we have multiple variables and incase if we have same data,then both will point out to the same box,they will not be getting the multiple boxes for 
+#each variable,and that's where python is more memory efficient
+
+id(10)#address is not based on the variable name,its based on the value itself
+k=10
+id(k)
+a=9
+id(9)
+id(b)
+k=a
+id(k)
+b=8
+PI =3.14 # PI value is constant,unfortunately we cannot make constant in python but we make it with our intention
+type(PI)
+
+
+
+
